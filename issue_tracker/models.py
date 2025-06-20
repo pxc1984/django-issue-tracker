50a2365a9c71c880fbef49a873ba5c23a963d007
pacable <igor.mamaev1@gmail.com>
2025-06-20 22:41:16 +0300

    Base models

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'projects'
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+class Issue(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        (0, 'OPEN'),
+        (1, 'NOT_PLANNED'),
+        (2, 'CLOSED'),
+    ]
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default=0)
+    PRIORITY_CHOICES = [
+        (0, 'LOW'),
+        (1, 'MEDIUM'),
+        (2, 'HIGH'),
+    ]
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'issues'
+        verbose_name = 'Issue'
+        verbose_name_plural = 'Issues'
+
+class Assignment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'assignments'
+        verbose_name = 'Assignment'
+        verbose_name_plural = 'Assignments'
+
+class ProjectMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    ROLE_CHOICES = [
+        (0, 'CONTRIBUTOR'),
+        (1, 'MANAGER'),
+    ]
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
+
+    class Meta:
+        db_table = 'memberships'
+        verbose_name = 'Project membership'
+        verbose_name_plural = 'Project memberships'
