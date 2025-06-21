c4931a4c58b74266cd78c80f76530e92047afc5c
pacable <igor.mamaev1@gmail.com>
2025-06-21 11:53:20 +0300

    Boilerplate code for views and urls.py

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def projects_view(request: HttpRequest) -> Response:
+    if request.method == 'GET':
+        ...
+    else: # 'POST'
+        ...
+
+
+@api_view(['GET', 'POST'])
+def project_members_view(request: HttpRequest, project_id: str) -> Response:
+    if request.method == 'GET':
+        ...
+    else: # 'POST'
+        ...
+
+
+@api_view(['GET', 'POST'])
+def project_issues_view(request: HttpRequest, project_id: str) -> Response:
+    if request.method == 'GET':
+        ...
+    else: # 'POST'
+        ...
+
+
+@api_view(['GET'])
+def issue_view(request: HttpRequest, project_id: str, issue_id: int) -> Response:
+    ...
