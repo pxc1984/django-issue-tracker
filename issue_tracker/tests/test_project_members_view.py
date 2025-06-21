ea90a7e06314f44d378ef3f35d2607fc199bab64
pacable <igor.mamaev1@gmail.com>
2025-06-21 14:15:11 +0300

    GET project members view done

---
+++ issue_tracker/tests/test_project_members_view.py
@@ -0,0 +1,67 @@
+﻿from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework.response import Response
+
+from issue_tracker.models import Project, ProjectMembership, ProjectPermission
+
+
+# noinspection PyTypeChecker
+class TestProjectMembersViewAPI(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.owner = User.objects.create_user(
+            username="Owner",
+            password='passwd123',
+        )
+        cls.user = User.objects.create_user(
+            username="Test user",
+            password='passwd124',
+        )
+
+        # Create a test project
+        cls.project = Project.objects.create(
+            name='TestProject',
+            description='This is project description',
+        )
+        ProjectMembership.objects.create(
+            user=cls.owner,
+            project=cls.project,
+            role=7,
+        )
+
+    def setUp(self):
+        self.client.force_authenticate(self.owner)
+
+    def testListMembersSuccess(self):
+        url = reverse('project members view', kwargs={'project_id': self.project.name})
+
+        response: Response = self.client.get(url, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['data']), 1)
+        self.assertEqual(response.data['data'][0]['username'], self.owner.username)
+        self.assertEqual(response.data['data'][0]['roles'], 'Owner')
+
+    def testAnonymousAccess(self):
+        self.client.force_authenticate(None)
+
+        url = reverse('project members view', kwargs={'project_id': self.project.name})
+
+        response: Response = self.client.get(url, {})
+        self.assertEqual(response.status_code, 403)
+
+    def testUnexistentProject(self):
+        url = reverse('project members view', kwargs={'project_id': 'qwery'})
+
+        response: Response = self.client.get(url, {})
+        self.assertEqual(response.status_code, 400)
+
+    def testNotEnoughPermissions(self):
+        self.client.force_authenticate(self.user)
+
+        url = reverse('project members view', kwargs={'project_id': self.project.name})
+
+        response: Response = self.client.get(url, {})
+        self.assertEqual(response.status_code, 403)
+
+
--- issue_tracker/views/project_members_view.py
+++ issue_tracker/views/project_members_view.py
@@ -1,12 +1,50 @@
-﻿from django.http import HttpRequest
+﻿from django.contrib.auth.models import User
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import *
+
+from issue_tracker.models import Project, ProjectMembership, ProjectPermission
 
 
 @api_view(['GET', 'POST'])
 def project_members_view(request: HttpRequest, project_id: str) -> Response:
-    if request.method == 'GET':
-        ...
-    else: # 'POST'
-        ...
+    """
+    Веб-сервис, отвечающий за работу с членами проекта.
 
+        GET -> Вывести список людей и их привилегий в проекте, если у запрашивающего есть доступ на Read в проекте
+        POST -> Изменить статус человека в проекте. Запрашивающий должен иметь право на Manage
+
+    :param request:
+    :param project_id:
+    :return:
+    """
+    if type(request.user) != User:
+        return Response({'message': 'You can\'t access this resource as an anonymous user'}, status=HTTP_403_FORBIDDEN)
+
+    project = Project.objects.filter(name=project_id).first()
+    if project is None:
+        return Response({'message': 'This project does not exist'}, status=HTTP_400_BAD_REQUEST)
+    membership = ProjectMembership.objects.filter(
+        user=request.user,
+        project=project,
+    ).first()
+
+    if request.method == 'GET':
+        return get_project_members_view(request, project, membership)
+    else: # 'POST'
+        return edit_project_members_view(request, project, membership)
+
+def get_project_members_view(request: HttpRequest, project: Project, membership: ProjectMembership) -> Response:
+    if not membership or not membership.role & ProjectPermission.Read.value:
+        return Response({'Not enough permissions to view this resource'}, status=HTTP_403_FORBIDDEN)
+
+    members = [{'username': membership.user.username, 'roles': ProjectPermission(membership.role).name} for membership in ProjectMembership.objects.filter(project=project)]
+
+    return Response({'data': members}, status=HTTP_200_OK)
+
+def edit_project_members_view(request: HttpRequest, project: Project, membership: ProjectMembership) -> Response:
+    if not membership or not membership.role & ProjectPermission.Manage.value:
+        return Response({'Not enough permissions to edit this resource'}, status=HTTP_403_FORBIDDEN)
+
+    raise NotImplementedError()
